b76ed294b5a1cca67f92c036d20f8386b1c694b1
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-02-25 18:00:57 -0300

    Adicionando movimentação ao personagem.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1;
+
+	private Rigidbody rb;
+
+	//chamado no primeiro frame quando o script é ativado
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	//chamado antes de cada frame ser renderizado
+	void Update()
+	{
+
+	}
+
+	//chamado antes de fazer um cálculo de physics
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
